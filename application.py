b51733d19ea810617096b40e155b8cc85b6b6343
ToanG1 <toandinh01675@gmail.com>
2024-03-05 21:09:10 +0700

    remove claude api

--- application.py
+++ application.py
@@ -2,7 +2,7 @@
 from json import JSONDecodeError
 import os
 from flask import Flask, request
-from services import claude, openAI, youtube, textModeration, imageModeration
+from services import openAI, youtube, textModeration, imageModeration
 from models.chatResponse import chatResponse
 import jsonpickle
 from werkzeug.utils import secure_filename
@@ -25,17 +25,12 @@ def allowed_file(filename):
 # Routes
 @app.route('/api/init', methods=['GET'])
 def init():
-    try:
-        return jsonpickle.encode(claude.initNewChat())
-    except:
-        return jsonpickle.encode(openAI.initNewChat())
+    return jsonpickle.encode(openAI.initNewChat())
 
 @app.route('/api/chat', methods=['POST'])
 def chat():
     try:
-        if (request.get_json()['c_id']):
-            return jsonpickle.encode(claude.chat(request.get_json()['prompt'], request.get_json()['c_id']))
-        elif (request.get_json()['messages']):
+        if (request.get_json()['messages']):
             return jsonpickle.encode(openAI.chat(request.get_json()['prompt'], request.get_json()['messages']))
         else :
             return jsonpickle.encode(chatResponse(code = 400, message ="Id and messages are required! Init new chat first",
@@ -44,30 +39,6 @@ def chat():
         return jsonpickle.encode(openAI.chat(request.get_json()['prompt'], openAI.initNewChat()))
     except:
         return jsonpickle.encode(chatResponse(code = 400, message ="Somethings missed or key reached limit", c_id= "",
-                                               messages=[], prompt= "", response= ""))
-
-@app.route('/api/file', methods=['POST'])
-def chatWithAttachment(): 
-    try:
-        if 'file' in request.files and request.form.get("c_id") and request.form.get("prompt"):
-            file = request.files['file']
-            if file or file.filename == '':
-                if allowed_file(file.filename):
-                    filename = secure_filename(file.filename)
-                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                    return jsonpickle.encode(claude.chatWithAttachment(request.form.get("prompt"),
-                                                os.path.join(app.config['UPLOAD_FOLDER'], filename), request.form.get("c_id")))
-                else:
-                    return jsonpickle.encode(chatResponse(code = 400, message ="Media file are not supported", c_id= "",
-                                                messages=[], prompt= "", response= ""))
-                
-        return jsonpickle.encode(chatResponse(code = 400, message ="No selected file!", c_id= "",
-                                                messages=[], prompt= "", response= ""))
-    except JSONDecodeError:
-        return jsonpickle.encode(chatResponse(code = 400, message ="Key reached limit", c_id= "",
-                                               messages=[], prompt= "", response= ""))
-    except :
-        return jsonpickle.encode(chatResponse(code = 400, message ="Somethings wrong", c_id= "",
                                                messages=[], prompt= "", response= ""))
 
 @app.route('/api/gen', methods=['POST'])
